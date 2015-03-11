3024af6e67ff6da6d8ea3a2816c723d681cab742
bash.py <bash.py@gmail.com>
2015-03-11 20:16:09 +0530

    Plain Commit

---
+++ pricescrap/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for pricescrap project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'pricescrap'
+
+SPIDER_MODULES = ['pricescrap.spiders']
+NEWSPIDER_MODULE = 'pricescrap.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'pricescrap (+http://www.yourdomain.com)'
