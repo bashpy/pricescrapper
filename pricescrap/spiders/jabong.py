b0d01e90e7ad16ce05a9b323d00ae28281389d01
bashpy <bash.py@gmail.com>
2015-03-19 11:02:45 +0530

    modified jabong spider

--- pricescrap/spiders/jabong.py
+++ pricescrap/spiders/jabong.py
@@ -2,24 +2,32 @@ __author__ = 'jovin'
 
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
-from pricescrap.items import Jabong
-
-from pricescrap.items import Jabong
+from pricescrap.items import Jabong_i
+import MySQLdb
+from pricescrap.items import Jabong_i
 
 class Jabong(BaseSpider):
-    """Spider for regularly updated livingsocial.com site, San Francisco Page"""
     name = "jabong"
     allowed_domains = ["flipkart.com"]
-    start_urls = ["http://www.flipkart.com/household/pressure-cookers-pans/prestige~brand/pr?sid=r4l,u3r&otracker=product_breadCrumbs_Prestige%20Pressure%20Cookers%20%26%20Pans#jumpTo=0|15"]
+    start_urls = ["http://www.flipkart.com/household/cookware/pots-pans/prestige~brand/pr?sid=r4l%2Cc7t%2Cqov&ref=f143ef7a-e313-461a-aea3-cd120ee9bc60"]
     def parse(self, response):
+        self.conn = MySQLdb.connect(user='root', passwd='2361250', db='test', host='localhost', charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
+        i = 0
+        price_list = []
+        title_list = []
         HtmlXPathSelector(response)
         hxs = HtmlXPathSelector(response)
+        titles = hxs.select("//a[@class='fk-display-block']/text()").extract()
         products = hxs.select("//div[@id='products']")
-        print products
         for l in products:
-            title = l.xpath('//title/text()').extract()
-            price = l.xpath('.//span[@class="fk-bold"]/text()').extract()
-            print title,price
+            item = Jabong_i()
+            items = []
+            items = l.xpath('.//span[@class="fk-font-17 fk-bold"]/text()').extract()
+        for j in titles:
 
+            self.cursor.execute("""INSERT INTO data (title,price) VALUES (%s,%s)""", (j,items[i]))
+            self.conn.commit()
+            i = i+1
 
 
