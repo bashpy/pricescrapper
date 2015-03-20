bade43d04b3c96774828b45766fc52d3984a91a4
bashpy <bash.py@gmail.com>
2015-03-20 11:26:41 +0530

    modified jabong spider

--- pricescrap/spiders/jabong.py
+++ pricescrap/spiders/jabong.py
@@ -5,11 +5,12 @@ import MySQLdb
 class Jabong(BaseSpider):
     name = "jabong"
     allowed_domains = "jabong.com"
-    start_urls = ["http://www.jabong.com/men/shoes/sneakers/?source=topnav_men"]
+    start_urls = ["http://www.jabong.com/men/shoes/"]
     def parse(self,response):
         self.conn = MySQLdb.connect(user='root', passwd='2361250', db='test', host='localhost', charset="utf8", use_unicode=True)
         self.cursor = self.conn.cursor()
         hxs = HtmlXPathSelector(response)
+        links = hxs.select("//@data-url").extract()
         content = hxs.select("//div[@class='box box-bgcolor']")
         price = content.select("//strong[@class='fs16 qa-price']/text()").extract()
         title = content.select(".//span[@class='qa-brandName title mt30 c999 prod-ellipsis']/text()").extract()
@@ -17,7 +18,11 @@ class Jabong(BaseSpider):
         l1 = [elem.strip().split(' ') for elem in title2]
         l2 = [elem.strip().split(' ') for elem in title]
         arr = []
+        new_links=[]
         i = 0
+        for l in links:
+            l = "www.jabong.com/"+l
+            new_links.append(l)
         for l in range(len(l2)):
             text = l2[i]+l1[i]
             arr.append(text)
@@ -34,7 +39,7 @@ class Jabong(BaseSpider):
             arlate.append(l)
         i = 0
         for j in price:
-            self.cursor.execute("""INSERT INTO data (title,price) VALUES (%s,%s)""", (arlate[i],j))
+            self.cursor.execute("""INSERT INTO data (title,price,link) VALUES (%s,%s,%s)""", (arlate[i],j,new_links[i]))
             self.conn.commit()
             i = i+1
-        # print arlate
+        print links
