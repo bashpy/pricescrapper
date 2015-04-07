e9b7741e6a985d74207dd142328b546dbfac4541
bashpy <bash.py@gmail.com>
2015-04-07 19:40:05 +0530

    add selenium extra functions

--- pricescrap/spiders/snapdeal_selenium.py
+++ pricescrap/spiders/snapdeal_selenium.py
@@ -4,27 +4,47 @@ from scrapy.spider import BaseSpider
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from scrapy.http import TextResponse
+from selenium.webdriver.common.keys import Keys
 import MySQLdb
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class Snapdeal(BaseSpider):
     name = "snapdeal1"
     allowed_domains = ["snapdeal.com"]
-    start_urls = ["http://www.snapdeal.com/search?keyword=nokia&santizedKeyword=nokia&catId=12&categoryId=336&suggested=true&vertical=p&noOfResults=20&clickSrc=searchOnSubCat&lastKeyword=nokia&prodCatId=133&changeBackToAll=true&foundInAll=false&categoryIdSearched=&cityPageUrl=&url=&utmContent=&catalogID=&dealDetail=#bcrumbSearch:nokia|bcrumbLabelId:133"]
+    start_urls = ["http://www.snapdeal.com//brand/micromax/mobiles-mobile-phones?"]
+
+
     def __init__(self):
         self.driver = webdriver.Firefox()
     def parse(self, response):
         self.driver.get(response.url)
-        copyright = self.driver.find_element_by_id('sdFooter')
-        ActionChains(self.driver).move_to_element(copyright).perform()
-
-        while not copyright.is_displayed():
-            copyright = self.driver.find_element_by_id('sdFooter')
-            time.sleep(25) #to let page content loading
+        copyright = self.driver.find_element_by_id('seeMoreProducts')
+        for i in  range(1,20):
             ActionChains(self.driver).move_to_element(copyright).perform()
+            time.sleep(15)
+        # delay = 3 # seconds
+        # try:
+        #     WebDriverWait(self.driver, delay).until(EC.presence_of_element_located(self.driver.find_element_by_id('seeMoreProducts')))
+        #     print "Page is ready!"
+        # except TimeoutException:
+        #     print "Loading took too much time!"
+        # self.driver.implicitly_wait(25)
+        # self.driver.execute_script("window.scrollTo(0,Math.max(document.documentElement.scrollHeight," + "document.body.scrollHeight,document.documentElement.clientHeight));")
+        # time.sleep(25)
+        # copyright = self.driver.find_element_by_id('seeMoreProducts')
+        # # copyright.send_keys(Keys.NULL)
+        # ActionChains(self.driver).move_to_element(copyright).perform()
+        # # #
+        # while not copyright.is_displayed():
+        #     copyright = self.driver.find_element_by_id('seeMoreProducts')
+        #     time.sleep(20) #to let page content loading
+        #     ActionChains(self.driver).move_to_element(copyright).perform()
 
         response = TextResponse(url=response.url, body=self.driver.page_source, encoding='utf-8')
-        links = response.xpath("//a[@class='hit-ss-logger somn-track prodLink hashAdded']/@href").extract()
-        price = response.xpath("//span[@id='price']").extract()
+        # links = response.xpath("//a[@class='hit-ss-logger somn-track prodLink hashAdded']/@href/text()").extract()
+        price = response.xpath("//div[@class='product_list_view_heading']/text()").extract()
 
 
         # links = self.driver.find_elements_by_class_name('productWrapper')
@@ -32,4 +52,3 @@ class Snapdeal(BaseSpider):
 
         # images = self.driver.find_element_by_xpath("//div[@class='productWrapper']//div[@class='outerImg']//@hoversrc").text
         # title = self.driver.find_element_by_xpath("//div[@class='productWrapper']//img/@alt").text
-
